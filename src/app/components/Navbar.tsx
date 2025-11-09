e432568af2077762540b5fbe8db1bda054865303
simonegarofalo <simonegarofalo96@gmail.com>
2025-11-09 20:19:56 +0100

    feat(navbar): implement responsive navbar component with dynamic route props

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,9 +1,16 @@
-import Link from '../components/Link.tsx'
+import Link from '../components/Link.tsx';
+import './Navbar.css'
 export default function Navbar() {
   return(
-  <ul>
-    <li><Link href="../blog">Blog</Link></li>
-      <li><Link href="../progetti">Progetti</Link></li>
-    </ul>
+    <header className="w-full grid grid-cols-12 h-14 border-b-[0.02rem]">
+      <nav className="col-start-2 col-end-12 flex items-center justify-between">
+        <div id="logo"></div>
+        <ul className="flex gap-2">
+          <li><Link href="../blog">blog</Link></li>
+          <li><Link href="../progetti">progetti</Link></li>
+          <li><Link href="" download>cv</Link></li>
+        </ul>
+      </nav>
+    </header>
   );
 };

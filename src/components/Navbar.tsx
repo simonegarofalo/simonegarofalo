215a2a5e19f3d806f5a5cae91a2f3fc4703d903f
simonegarofalo <simonegarofalo96@gmail.com>
2025-11-22 01:03:11 +0100

    feat(ui): update logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,7 +5,7 @@ export default function Navbar() {
   return(
     <header className="w-full grid grid-cols-12 h-14 border-b-[0.02rem]">
       <nav className="col-start-2 col-end-12 md:col-start-4 md:col-end-10 flex items-center justify-between">
-        <Link href="/"><div id="logo"></div></Link>
+        <Link href="/"><p className="text-2xl">../</p></Link>
         <ul className="flex items-center justify-center gap-4">
           <li><Link href="/blog">posts</Link></li>
           <li><Link href="/projects">repos</Link></li>
